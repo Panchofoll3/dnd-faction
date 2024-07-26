649f5a152a93f78103dda3a493dc5ffc9c0a91c0
Panchofoll3 <Micanoccioline@gmail.com>
2024-07-26 13:08:55 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Aggiungi eventuali funzionalità interattive qui
+function checkPassword() {
+    const password = document.getElementById('password').value;
+    const correctPassword = 'password_segreta'; // Cambia questa password con quella che desideri
+
+    if (password === correctPassword) {
+        document.getElementById('password-container').style.display = 'none';
+        document.getElementById('secret-content').style.display = 'block';
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+    }
+}
